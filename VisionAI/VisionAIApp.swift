b35109ac236b723361f7a2513a07f7abd77f75ee
Baran Haydar <baranhaydar80@gmail.com>
2026-03-22 19:49:46 +0300

    Initial Commit

---
+++ VisionAI/VisionAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionAIApp.swift
+//  VisionAI
+//
+//  Created by Baran on 22.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct VisionAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
